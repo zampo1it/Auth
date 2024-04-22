ee8c75b91d4ca9ca4be3a00765f9fe5e922533a7
zampo1it <zampo1it@yandex.ru>
2024-04-22 16:28:44 +0400

    first commit

---
+++ Model/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp5.Model
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ViewModel/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+
+namespace WebApp5.ViewModel
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
